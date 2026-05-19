761ea48ade1c4dd4efa5372091e31bab61acaeae
Tomislav Novosel <novosel.tomislav86@gmail.com>
2026-05-19 23:09:15 +0200

    Connect Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    projectId: "g93mof",
   e2e: {
     baseUrl: "https://r1036223-realbeans.myshopify.com",
     supportFile: "cypress/support/e2e.js",
